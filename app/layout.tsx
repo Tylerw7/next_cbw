19955ab8ef1e2df41a63577ceaeaddecd086469e
Tylerw7 <t4wettach@gmail.com>
2025-09-30 19:23:11 -0400

    fixed for fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter, Merriweather } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
 
@@ -15,10 +15,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-const merriweather = Merriweather({
-  weight: ["400", "700"], 
-  subsets: ["latin"],
-})
+
 
 const inter = Inter({
   variable: "--font-inter",

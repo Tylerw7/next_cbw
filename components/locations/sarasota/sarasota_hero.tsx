cfac61cc4d6b308beb51d6d73b1e9937ff2c1d1c
Tylerw7 <t4wettach@gmail.com>
2025-10-01 16:55:09 -0400

    installed framer-motion

--- components/locations/sarasota/sarasota_hero.tsx
+++ components/locations/sarasota/sarasota_hero.tsx
@@ -4,8 +4,12 @@ import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Raleway } from "next/font/google";
 
-
+// Raleway Font variable
 const raleway = Raleway({ subsets: ["latin"], weight: ["400", "700"] });
+
+
+
+
 
 
 const SarasotaHero = () => {
@@ -33,11 +37,11 @@ const SarasotaHero = () => {
     <div className="relative overflow-hidden p-[10px] w-full h-[85vh] bg-cover bg-center flex flex-col justify-center items-start text-center" style={{ backgroundImage: 'url("/images/Sarasota-hero.jpg")' }}>
       
         {/* Content */}
-        <div className="w-[50vw] flex flex-col ">
+        <div className="w-[45vw] flex flex-col ">
             <h1 className={raleway.className + " text-[3.5rem] pl-[50px] font-bold text-start"}>Solving Your Cabinet And Furniture Needs In Sarasota, FL</h1>
         </div>
         <div className="w-[175px] h-[7px] bg-[#5a8495] ml-[50px] mt-[25px]"></div>
-        <p className="ml-[50px] mt-[25px] text-[1.5rem] text-start w-[50vw]">We specialize in premium cabinetry for every space in your home, from kitchens and bathrooms to custom built-in closets, entertainment centers, shelving, and more.</p>
+        <p className={raleway.className + " ml-[50px] mt-[25px] text-[1.5rem] text-start w-[45vw]"}>We specialize in premium cabinetry for every space in your home, from kitchens and bathrooms to custom built-in closets, entertainment centers, shelving, and more.</p>
         <Button className="
             ml-[50px] 
             mt-[50px] 

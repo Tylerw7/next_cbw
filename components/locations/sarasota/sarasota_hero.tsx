184844c76399ee81033027ab293566f8be84342b
Tylerw7 <t4wettach@gmail.com>
2025-10-01 13:36:30 -0400

    fixed font to raleway

--- components/locations/sarasota/sarasota_hero.tsx
+++ components/locations/sarasota/sarasota_hero.tsx
@@ -1,8 +1,8 @@
 
-import { Merriweather } from "next/font/google";
+import { Raleway } from "next/font/google";
 
 
-const merriweather = Merriweather({ subsets: ["latin"], weight: "400" });
+const raleway = Raleway({ subsets: ["latin"], weight: ["400", "700"] });
 
 
 const SarasotaHero = () => {
@@ -11,13 +11,13 @@ const SarasotaHero = () => {
 
 
     return ( 
-    <div className="relative overflow-hidden p-[10px] w-full min-h-screen bg-cover bg-center flex flex-col justify-center items-start text-center" style={{ backgroundImage: 'url("/images/Sarasota-hero.jpg")' }}>
+    <div className="relative overflow-hidden p-[10px] w-full h-[85vh] bg-cover bg-center flex flex-col justify-center items-start text-center" style={{ backgroundImage: 'url("/images/Sarasota-hero.jpg")' }}>
         {/* Overlay to dim the background */}
         {/*<div className="absolute inset-0 bg-black/40"></div>*/}
       
         {/* Content */}
         <div className="w-[50vw] flex flex-col ">
-            <h1 className={merriweather.className + "text-[4rem] p-4 font-bold "}>Solving your cabinet and furniture needs in Sarasota, FL</h1>
+            <h1 className={raleway.className + " text-[3.5rem] pl-[50px] font-bold"}>Solving your cabinet and furniture needs in Sarasota, FL</h1>
         </div>
     </div>
      );

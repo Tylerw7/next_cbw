56734b27ca979bc6c0f21a01010424e388d69d73
Tylerw7 <t4wettach@gmail.com>
2025-10-11 09:54:15 -0400

    updated sarasota call to action

--- components/locations/sarasota/call-to-action.tsx
+++ components/locations/sarasota/call-to-action.tsx
@@ -1,12 +1,30 @@
 
 //#a8cbda
+import { MapPin, Contact, CalendarCheck } from "lucide-react"
+import { Button } from "@/components/ui/button"
 
 const CallToAction = () => {
   return (
-    <div className="w-full ">
-        <div className="w-[300px] h-[300px]">
+    <div className="w-full flex flex-wrap justify-evenly">
 
+        <div className="w-[300px] h-[300px] flex flex-col justify-center items-center gap-2">
+            <MapPin size={80} color="#5a8495"/>
+            <h3 className="font-bold text-[1.5rem]">Location</h3>
+            <p className="text-center">1746 10th Way Unit B Sarasota, FL 34236</p>
         </div>
+
+        <div className="w-[300px] h-[300px] flex flex-col justify-center items-center gap-2">
+            <Contact size={80} color="#5a8495"/>
+            <h3 className="font-bold text-[1.5rem]">Contact</h3>
+            <p className="text-center">941-232-2817</p>
+        </div>
+
+        <div className="w-[300px] h-[300px] flex flex-col justify-center items-center gap-2">
+            <CalendarCheck size={80} color="#5a8495"/>
+            <h3 className="font-bold text-[1.5rem]">Schedule Consultation</h3>
+            <Button className="">Find Out More</Button>
+        </div>
+
     </div>
   )
 }
